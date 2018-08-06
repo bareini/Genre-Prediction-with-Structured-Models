2876da19bef342bbc10181be4fc6342d61ef1007
bareini <briany202@gmail.com>
2018-08-06 14:08:19 +0300

    modifying trainning feature matrices. relevant funcations viterbi

--- model.py
+++ model.py
@@ -19,13 +19,18 @@ class Model:
         """
         self.type = 'MEMM'
         self.train_feature_matrix = None
-        self.train_feature_all_posible_labels = None
+
+        # functions as words_features_matrix - a dict which saves all possible labels for node
+        self.possible_generes_per_node_matrix = {}
         # todo: decide where to initial this
         self.all_tags_list = None
-        self.build_features_head_modifier(df_demo, df_x)
+
+        # functions as build_features_head_modifier
+        self.build_features_matrices(df_demo, df_x)
         self.device_indexes_map = df_x.groupby(config.x_device_id).groups
 
-    def build_features_head_modifier(self, df_demo, df_x):
+
+    def build_features_matrices(self, df_demo, df_x):
         """
 
         :param df_demo:
@@ -42,7 +47,11 @@ class Model:
 
         relevant_demo = None
         device_id = None
+        prev1_node_label = None
+        prev2_node_label = None
+
         for node in x_matrix:
+
             node_index = df_cols_dict['x'][config.x_row_index]
             node_label = df_cols_dict['x'][config.x_label]
 
@@ -50,53 +59,117 @@ class Model:
                 device_id = node[df_cols_dict['x'][config.x_device_id]]
                 relevant_demo = df_demo.query("{}=={}".format(config.demo_device_id, device_id)).values
 
+
+            # indexes for the training ('truth') matrix
+            training_matrix_rows_index_counter = 0
+            training_matrix_rows_index = []
+            training_matrix_columns_index = []
+
+            # indexes for possible labels matrix
+            word_matrix_rows_index_counter = 0
             word_matrix_rows_index = []
             word_matrix_columns_index = []
 
-            for possible_genre in self.all_tags_list:
 
+
+            for genere_counter, possible_genre in enumerate(self.all_tags_list):
+
+                #
                 columns_index = []
                 # todo: create a feature class
                 # todo: create the feature position builder
-                current_features = self.features.create_features(modifier, possible_head, tree)
+                # todo: fearture creater that returns the relevant indexes
+                #     current_features = self.features.create_features(modifier, possible_head, tree)
+                #     for feature in current_features:
+                #         if feature in self.features_position:
+                #             word_matrix_rows_index.append(possible_genre.counter)
+                #             columns_index.append(self.features_position[feature])
+                #
+                #     word_matrix_columns_index += columns_index
+                #
+                #     # True label
+                #     if node[node_index] == node[node_label]:
+                #         rows_index = np.zeros(shape=len(columns_index), dtype=np.int32)
+                #         cols_index = np.asarray(a=columns_index, dtype=np.int32)
+                #         data_to_insert = np.ones(len(columns_index), dtype=np.int8)
+                #         train_word_vector = csr_matrix((data_to_insert, (rows_index, cols_index)),
+                #                                        shape=(1, self.feature_vector_len))
+                #         self.training_features_matrix[(sentence_num, modifier.counter)] = train_word_vector
+                #
+                # rows_index = np.asarray(a=word_matrix_rows_index, dtype=np.int32)
+                # cols_index = np.asarray(a=word_matrix_columns_index, dtype=np.int32)
+                # data_to_insert = np.ones(len(word_matrix_rows_index), dtype=np.int8)
+                # word_matrix = csr_matrix((data_to_insert, (rows_index, cols_index)),
+                #                          shape=(len(tree), self.feature_vector_len))
+
+
+                # used to be self.features.create_features
+                current_features = self.potetial_features(node, prev1_node_label, prev2_node_label, possible_genre)
+
                 for feature in current_features:
                     if feature in self.features_position:
-                        word_matrix_rows_index.append(possible_genre.counter)
+                        # the indecator location in the mini matrix - row (same for all) and column (the features from the map)
+                        word_matrix_rows_index.append(genere_counter)
                         columns_index.append(self.features_position[feature])
 
                 word_matrix_columns_index += columns_index
 
-                # True label
-                if node[node_index] == node[node_label]:
-                    rows_index = np.zeros(shape=len(columns_index), dtype=np.int32)
-                    cols_index = np.asarray(a=columns_index, dtype=np.int32)
-                    data_to_insert = np.ones(len(columns_index), dtype=np.int8)
-                    train_word_vector = csr_matrix((data_to_insert, (rows_index, cols_index)),
-                                                   shape=(1, self.feature_vector_len))
-                    self.training_features_matrix[(sentence_num, modifier.counter)] = train_word_vector
+                if possible_genre == node_label:
+
+                    temp = list(np.ones(len(columns_index), dtype=np.int32) * training_matrix_rows_index_counter)
+                    training_matrix_rows_index += temp
+                    training_matrix_columns_index += columns_index
+                    training_matrix_rows_index_counter += 1
 
             rows_index = np.asarray(a=word_matrix_rows_index, dtype=np.int32)
             cols_index = np.asarray(a=word_matrix_columns_index, dtype=np.int32)
             data_to_insert = np.ones(len(word_matrix_rows_index), dtype=np.int8)
-            word_matrix = csr_matrix((data_to_insert, (rows_index, cols_index)),
-                                     shape=(len(tree), self.feature_vector_len))
+            possible_genre_matrix = csr_matrix((data_to_insert, (rows_index, cols_index)),
+                                     shape=(len(self.tags_seen_in_train), self.feature_vector_len))
 
-            self.words_features_matrix[(sentence_num, modifier.counter)] = word_matrix
+            # Initialize the dict where key : (sentence,word), value : word_matrix
+            # original keys - [(device_id, node_index)] - BUT assuming that for each view we have a unigue index
+
+            self.possible_generes_per_node_matrix[ node_index] = possible_genre_matrix
+
+            prev2_node_label = prev1_node_label
+            prev1_node_label = node_label
+
+        rows_index = np.asarray(a=training_matrix_rows_index, dtype=np.int32)
+        cols_index = np.asarray(a=training_matrix_columns_index, dtype=np.int32)
+        data_to_insert = np.ones(len(training_matrix_rows_index), dtype=np.int8)
+        self.training_matrix = csr_matrix((data_to_insert, (rows_index, cols_index)),
+                                              shape=(training_matrix_rows_index_counter, self.feature_vector_len))
+
+        logger.info("create_features_vector_for_train <-------------- ")
 
         logger.debug("DependencyParsing: build_features_head_modifier <--------------")
 
 
-
-
-
-
-    def feature_builder(self, feature_list):
+    def potetial_features(node, prev1_node_label, prev2_node_label, target_genere):
         """
 
-        :param list(int) feature_list:
+        :param prev1_node_label: genre of the previous view - string
+        :param prev2_node_label:  genre of the 2-previous view - string
+        :param target_genere:  the genere we are considering
+        :return: a list of string which represents the potential - features for verifying weather its
+        """
+        # todo: take feature formats from dafna's code
+
+    def feature_vec_builder(self, feature_list):
+
+        """
+        :param list(string) feature_list: potential features names
         :return:
         """
-        pass
+        feature_vec = np.zeros(len(self.feature_position))
+
+        for feature in feature_list:
+            if feature in self.features_position:
+                feature_vec[self.features_position[feature]] = 1
+
+        return feature_vec
+
 
 
 if __name__ == '__main__':
--- viterbi.py
+++ viterbi.py
@@ -7,6 +7,100 @@ from scipy.optimize import fmin_l_bfgs_b
 from sklearn.metrics import accuracy_score, confusion_matrix as conf_matrix
 
 
+def find_feature_vector_test_set(self, sentence, k, t, u, v):
+    """
+    Find all the places in feature vector where the entries are 1
+    :param sentence: list of word
+    :param k: current position
+    :param t: tag in position k-2
+    :param u: tag in position k-1
+    :param v: tag in position k
+    :return:
+    """
+
+    position_in_vector = []
+    origin_word = sentence[k - 1]
+    word = origin_word.lower()
+
+    # word x tag
+    pair = word + "_" + v + "_100"
+    if pair in self.features_position:
+        position_in_vector.append(self.features_position[pair])
+    # tag trigram
+    trigram = t + "_" + u + "_" + v + "_103"
+    if trigram in self.features_position:
+        position_in_vector.append(self.features_position[trigram])
+    # tag bigram
+    bigram = u + "_" + v + "_104"
+    if bigram in self.features_position:
+        position_in_vector.append(self.features_position[bigram])
+
+    if self.model_type == "advanced":
+        # tag unigram
+        unigram = v + "_105"
+        if unigram in self.features_position:
+            position_in_vector.append(self.features_position[unigram])
+
+        # prefix <=4 x tag
+        prefix_list = [word[:i] + '_' + v + "_101" for i in range(1, 5)]
+        prefix_list = list(set(prefix_list))
+        for item in prefix_list:
+            if item in self.features_position:
+                position_in_vector.append(self.features_position[item])
+
+        # suffix <=4 x tag
+        suffix_list = [word[i:] + '_' + v + "_102" for i in range(-4, 0)]
+        suffix_list = list(set(suffix_list))
+        for item in suffix_list:
+            if item in self.features_position:
+                position_in_vector.append(self.features_position[item])
+
+        # current tag with previous word
+        if u != "*":
+            previous_word_current_tag = sentence[k - 2] + "_" + v + "_106"
+            if previous_word_current_tag in self.features_position:
+                position_in_vector.append(self.features_position[previous_word_current_tag])
+        # current tag with next word
+        if k != (len(sentence)):
+            next_word_current_tag = sentence[k] + "_" + v + "_107"
+            if next_word_current_tag in self.features_position:
+                position_in_vector.append(self.features_position[next_word_current_tag])
+        # is the string a number
+        if str.isdigit(word):
+            position_in_vector.append(self.features_position["is_numeric"])
+        # is the string start with upper letter
+        if str.isupper(origin_word[0]):
+            position_in_vector.append(self.features_position["is_capitalized"])
+
+    return position_in_vector
+
+
+
+def probability(self, Sk_2, Sk_1, Sk, sentence, k):
+    """
+    :param Sk_2:
+    :param Sk_1:
+    :param Sk:
+    :param sentence:
+    :param k: the current position
+    :return:
+    """
+
+    probability_table = defaultdict(tuple)
+    weights = self.v
+
+    for t in Sk_2:
+        for u in Sk_1:
+            for v in Sk:
+                probability_table[(t, u, v)] = np.exp(
+                    sum(weights[self.find_feature_vector_test_set(sentence, k, t, u, v)]))
+
+            # Constant Denominator
+            denominator = np.sum(probability_table[(t, u, v)] for v in Sk)
+            for v in Sk:
+                probability_table[(t, u, v)] /= denominator
+
+    return probability_table
 
 def viterbi_algorithm(self, sequence, label_in_train, ):
     """
