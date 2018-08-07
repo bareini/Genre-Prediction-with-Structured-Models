982e0f99fc4a5a1df4510500bcbb5c7b25f89902
bareini <briany202@gmail.com>
2018-08-07 10:16:47 +0300

    modifying trainning feature matrices. relevant funcations viterbi

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ import numpy as np
 from collections import Counter
 import config
 
+
 class Model:
     """
     the model builder
@@ -33,7 +34,6 @@ class Model:
         self.tags_seen_in_train = []
         self.feature_vector_len = 0
         self.features_position = []
-
 
     def build_features_matrices(self, df_demo, df_x):
         """
@@ -68,7 +68,6 @@ class Model:
                 device_id = node[df_cols_dict['x'][config.x_device_id]]
                 relevant_demo = df_demo.query("{}=={}".format(config.demo_device_id, device_id)).values
 
-
             # indexes for the training ('truth') matrix
             training_matrix_rows_index_counter = 0
             training_matrix_rows_index = []
@@ -78,8 +77,6 @@ class Model:
             word_matrix_rows_index_counter = 0
             word_matrix_rows_index = []
             word_matrix_columns_index = []
-
-
 
             for genere_counter, possible_genre in enumerate(self.all_tags_list):
 
@@ -124,7 +121,6 @@ class Model:
                 word_matrix_columns_index += columns_index
 
                 if possible_genre == node_label:
-
                     temp = list(np.ones(len(columns_index), dtype=np.int32) * training_matrix_rows_index_counter)
                     training_matrix_rows_index += temp
                     training_matrix_columns_index += columns_index
@@ -134,12 +130,12 @@ class Model:
             cols_index = np.asarray(a=word_matrix_columns_index, dtype=np.int32)
             data_to_insert = np.ones(len(word_matrix_rows_index), dtype=np.int8)
             possible_genre_matrix = csr_matrix((data_to_insert, (rows_index, cols_index)),
-                                     shape=(len(self.tags_seen_in_train), self.feature_vector_len))
+                                               shape=(len(self.tags_seen_in_train), self.feature_vector_len))
 
             # Initialize the dict where key : (sentence,word), value : word_matrix
             # original keys - [(device_id, node_index)] - BUT assuming that for each view we have a unigue index
 
-            self.possible_generes_per_node_matrix[ node_index] = possible_genre_matrix
+            self.possible_generes_per_node_matrix[node_index] = possible_genre_matrix
 
             prev2_node_label = prev1_node_label
             prev1_node_label = node_label
@@ -148,14 +144,13 @@ class Model:
         cols_index = np.asarray(a=training_matrix_columns_index, dtype=np.int32)
         data_to_insert = np.ones(len(training_matrix_rows_index), dtype=np.int8)
         self.train_feature_matrix = csr_matrix((data_to_insert, (rows_index, cols_index)),
-                                              shape=(training_matrix_rows_index_counter, self.feature_vector_len))
+                                               shape=(training_matrix_rows_index_counter, self.feature_vector_len))
 
         logger.info("create_features_vector_for_train <-------------- ")
 
         logger.debug("DependencyParsing: build_features_head_modifier <--------------")
 
-
-    def potetial_features( self, node, prev1_node_label, prev2_node_label, target_genere):
+    def potetial_features(self, node, prev1_node_label, prev2_node_label, target_genere):
         """
 
         :param prev1_node_label: genre of the previous view - string
